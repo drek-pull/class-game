5f49be54b3fd3c4cc8d07932f3e483af96f2367c
archanaa131 <84088255+archanaa131@users.noreply.github.com>
2021-05-17 11:02:43 -0500

    Add files via upload

--- com.APCS.compSciGame/Room1.java
+++ com.APCS.compSciGame/Room1.java
@@ -10,23 +10,25 @@ public class Room1 extends Room
     {
         System.out.println("Welcome to "+name+"!");
         System.out.println("In order to make it to the next room, you have to go through all of the obstacles in this room.");
-        System.out.println("riddle");
+        System.out.println("What has to be broken, before you use it?");
+        
 
     }
 
     @Override
     public void getPuzzleAnswers()
     {
-        while(!escape)
+        answer = s.nextLine();
+        while(escape)
         {
-            answer = s.nextLine();
-            if(answer.equals("jdsfsdf"))
+            if(answer.equals("An egg"))
             {
                 escape = true;
             }
             else
             {
                 System.out.println("That is incorrect! Keep trying... that is the only way you can escape this room.");
+                Puzzle();
             }
         }
         System.out.println("Congrats!!! You made it out! But you aren't done, time to move onto room 2...");
