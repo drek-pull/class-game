52f3ebdfaf31e3a33687d1ecb36bc6842c6fcf9c
archanaa131 <84088255+archanaa131@users.noreply.github.com>
2021-05-17 10:52:34 -0500

    Add files via upload

--- com.APCS.compSciGame/Room.java
+++ com.APCS.compSciGame/Room.java
@@ -1,13 +1,9 @@
-import lib.*;
-/**
- * Code used by most types of characters.
- * Will be removed if we decide to not create multiple characters.
- *
- * @author Derek P. (Circle Onyx)
- * @version 1.0
- */
+
 public abstract class Room
 {
     String name;
+   
     public abstract void Puzzle();
+
+    public abstract void getPuzzleAnswers();
 }
