c42fed5f665671f768f1847b9a5a62093b6a511e
sanikanav <84088529+sanikanav@users.noreply.github.com>
2021-05-17 11:02:45 -0500

    Add files via upload

---
+++ com.APCS.compSciGame/Room2.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+public class Room2 extends Room
+{
+    Scanner s = new Scanner(System.in);
+    String name = "Room 2";
+    String answer = "";
+    boolean escape = false;
+    @Override
+    public void Puzzle()
+    {
+        System.out.println("Welcome to "+name+"!");
+        System.out.println("In order to make it to the next room, you have to go through all of the obstacles in this room.");
+        System.out.println("Riddle: The more you take, the more you leave behind. What am I?");
+
+    }
+
+    @Override
+    public void getPuzzleAnswers()
+    {
+        while(!escape)
+        {
+            answer = s.nextLine();
+            if(answer.equals("footsteps"))
+            {
+                escape = true;
+            }
+            else
+            {
+                System.out.println("That is incorrect! Keep trying... that is the only way you can escape this room.");
+            }
+        }
+        System.out.println("Congrats!!! You made it out! But you aren't done, time to move onto room 2...");
+    }
+}
