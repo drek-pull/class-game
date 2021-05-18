d5fc6d1296a886648890355babf0ac99e746b2e6
drek-pull <84030827+drek-pull@users.noreply.github.com>
2021-05-18 09:55:43 -0500

    Runner edited

--- com.APCS.compSciGame/Runner.java
+++ com.APCS.compSciGame/Runner.java
@@ -8,6 +8,14 @@
 public class Runner
 {
     public static void Main(String[] args){
-        
+        boolean key = false;
+        Room r = new Room1();
+        Hallway h = new Hallway(r,r);
+        while(!key){
+            Room roo = h.Puzzle();
+            if(roo != null){
+                roo.Puzzle();
+            }
+        }
     }
 }
